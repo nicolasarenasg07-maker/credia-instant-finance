6adb6e6ac2b202fdce1ec7ffac554408c1ac2ca9
inesjava07 <ijavaloyes.ieu2025@student.ie.edu>
2026-02-06 18:54:13 +0100

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { supabase } from "@/lib/supabase";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
